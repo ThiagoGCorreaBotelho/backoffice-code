5aa112da36371aadb8ca1d8bc4d75b1b2555bda8
Thiago Corrêa <thiagogcorreabotelho@outlook.com>
2021-10-10 10:15:10 -0300

    #1
    Transportar layout administrativo AdminLTE e deixar padronizado com as pastas de viewcomponent para ser renderizadas dentro do layout.

---
+++ BackOfficeCode/Web_Application/ViewComponents/VcHeader.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackOffice_Application.ViewComponents
+{
+    public class VcHeader : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
